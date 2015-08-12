16456dc4d73126469c244a5e95cdae3851bb4977
Ononos <anonym107@gmail.com>
2015-08-12 11:45:38 +0300

    utf8 sane check

--- fix_meta.pl
+++ fix_meta.pl
@@ -3,6 +3,7 @@ use utf8;
 use strict;
 use Encode;
 use Encode::Guess;
+use charnames ':full';
 
 use open ':std', ':encoding(UTF-8)';
 
@@ -30,6 +31,34 @@ s = map_metadata(fix_meta, s)
 ';
 }
 
+sub is_sane_utf8($;$)
+{
+  my $string = shift;
+
+  my $re_bit = join "|", map { Encode::encode("utf8",chr($_)) } (127..255);
+
+  # regexp in scalar context with 'g', meaning this loop will run for
+  # each match.  Should only have to run it once, but will redo if
+  # the failing case turns out to be allowed in %allowed.
+  while ($string =~ /($re_bit)/o)
+  {
+    # work out what the double encoded string was
+    my $bytes = $1;
+
+    my $index = $+[0] - length($bytes);
+    my $codes = join '', map { sprintf '<%00x>', ord($_) } split //, $bytes;
+
+    # what character does that represent?
+    my $char = Encode::decode("utf8",$bytes);
+    my $ord  = ord($char);
+    my $hex  = sprintf '%00x', $ord;
+    $char = charnames::viacode($ord);
+
+    return 0;
+  }
+
+  return 1;
+}
 # Return (artist, title) base on input (file, artist, title)
 # If no artist and no title than try return title from filename.
 #
@@ -72,10 +101,14 @@ sub tag_encode {
 my $arg = $ARGV[0];
 chomp($arg);
 
-# liquidsoap give meta in utf8. need latin1
-Encode::from_to ($arg, "UTF-8", "iso-8859-1");
-
 my ($filename, $artist, $title) = split('::', $arg);
+
+# liquidsoap give meta in utf8. need latin1
+unless (is_sane_utf8(qq{$artist $title})) {
+    Encode::from_to ($artist, "UTF-8", "iso-8859-1");
+    Encode::from_to ($title, "UTF-8", "iso-8859-1");
+}
+#Encode::from_to ($arg, "UTF-8", "iso-8859-1");
 
 ($artist, $title) = tag_encode $filename, $artist, $title;
 
